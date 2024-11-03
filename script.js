55981818a3e5e72d10bddf75deb684432af87849
CarlosAlves84 <cenorinha2013@gmail.com>
2024-11-03 19:04:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Função para fechar o pop-up
+function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
+    document.body.style.overflow = "auto"; // Permite o scroll após fechar o pop-up
+}
+
+// Exibe o pop-up automaticamente ao abrir o site
+window.onload = function() {
+    document.getElementById("popupOverlay").style.display = "flex";
+};
